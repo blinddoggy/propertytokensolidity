ec3637ecb8f182887b84632f17b202a8dd5ea984
blinddoggy <elrobotdelpueblo@gmail.com>
2023-05-11 20:51:57 -0500

    cleaning

--- contracts/Property.sol
+++ contracts/Property.sol
@@ -9,9 +9,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 //import "@openzeppelin/contracts/token/BEP20/BEP20.sol";
-
-
 
 interface IBEP20 {
     function balanceOf(address) external view returns (uint256);
@@ -19,29 +18,44 @@ interface IBEP20 {
 
 
 
+
 /// @custom:security-contact cucarachatraicionera@gmail.com
-contract PropertyToken is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable, ERC721Burnable {
+contract Property is
+    ERC721,
+    ERC721Enumerable,
+    ERC721URIStorage,
+    Pausable,
+    Ownable,
+    ERC721Burnable
+{
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("VersallesFincas", "VSF") {}
-
-
     string ipfsHash;
-    
-    function sendHash(string memory x) public {
-        ipfsHash = x;
+    address tokenAddress = 0x242a1fF6eE06f2131B7924Cacb74c7F9E3a5edc9;
+
+    constructor(
+        string memory nombre,
+        string memory simbolo,
+        string memory _ipfsHash
+    ) ERC721(nombre, simbolo) {
+        ipfsHash = _ipfsHash;
     }
-    
+
     function getHash() public view returns (string memory) {
         return ipfsHash;
     }
 
+    //easy concatenate
+    function concatenate(string memory a,string memory b) public pure returns (string memory){
+        return string(bytes.concat(bytes(a), bytes(b)));
+    } 
 
-
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://www.ngenespanol.com/wp-content/uploads/2023/02/jardines-de-versalles.jpg";
+    function _baseURI() internal view override returns (string memory) {
+        string memory url = "https://ipfs.io/ipns/";
+        string memory hash = getHash();
+        return concatenate(url,hash);
     }
 
     function pause() public onlyOwner {
@@ -51,7 +65,7 @@ contract PropertyToken is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, 
     function unpause() public onlyOwner {
         _unpause();
     }
-    
+
     function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
@@ -59,51 +73,63 @@ contract PropertyToken is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, 
         _setTokenURI(tokenId, uri);
     }
 
-     struct Proyecto{
-            string name;
-            string symbol;
-            uint256 amount;
-            string img;
-            uint256 balance;
-        }
+    
+
+    struct Proyecto {
+        string name;
+        string symbol;
+        uint256 amount;
+        string img;
+        uint256 balance;
+    }
 
     //function returning struct
-    function getStruct(address contractProjectAddress) public view returns(Proyecto memory){
+    function getproject(address contractProjectAddress)
+        public
+        view
+        returns (Proyecto memory)
+    {
         address owner = owner();
-        address tokenAddress = 0x242a1fF6eE06f2131B7924Cacb74c7F9E3a5edc9;
-        return Proyecto(name(),symbol(),balanceNfts(owner),tokenURI(0),balanceToken(tokenAddress,contractProjectAddress)); 
+        return
+            Proyecto(
+                name(),
+                symbol(),
+                balanceNfts(owner),
+                tokenURI(0),
+                address(this).balance
+            );
     }
-         
+
     //balance token ERC20 contract
-    function balanceToken(address tknDdrss,address ddrss) public view returns(uint256){
+    function balanceToken(address tknDdrss, address ddrss)
+        public
+        view
+        returns (uint256)
+    {
         return IERC20(tknDdrss).balanceOf(ddrss);
         //return IBEP20(tknDdrss).balanceOf(ddrss);
     }
 
     //balanace nft contract
-    function balanceNfts(address ddrss) public view returns(uint256){
+    function balanceNfts(address ddrss) public view returns (uint256) {
         return balanceOf(ddrss);
     }
-    //name project
-    function nombresiio() public view returns(string memory){
-        return name();
-    }
-    //symbol project
-    function symbolProject() public view returns(string memory){
-        return symbol();
-    }
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
-        internal
-        whenNotPaused
-        override(ERC721, ERC721Enumerable)
-    {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize
+    ) internal override(ERC721, ERC721Enumerable) whenNotPaused {
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
         super._burn(tokenId);
     }
 
@@ -124,9 +150,4 @@ contract PropertyToken is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, 
     {
         return super.supportsInterface(interfaceId);
     }
-
-
-    
-
-
 }
